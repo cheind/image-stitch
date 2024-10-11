f7c851c27f779c4e7ec5bcc80e7a0e2bb5befa41
Christoph Heindl <christoph.heindl@gmail.com>
2024-10-11 20:46:48 +0200

    updated demo

--- oof.py
+++ oof.py
@@ -11,91 +11,68 @@ from pathlib import Path
 
 import image_stitch as ist
 
+from omegaconf import OmegaConf
+
+
+def compute_weights_by_color(cfg: OmegaConf, imgs: np.ndarray):
+
+    imgs_lab = np.stack([cv2.cvtColor(i, cv2.COLOR_BGR2LAB) for i in imgs], 0)
+
+    yellow = np.array([97.17, -21.29, 94.41])
+    d = np.linalg.norm(imgs_lab - yellow.reshape(1, 1, 1, 3), axis=-1, keepdims=True)
+
+    tol = 60
+    w = np.exp(-np.maximum((d - tol), 0) * 0.1)
+
+    return w
+
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-n", type=int, help="Use first N images", default=-1)
-    parser.add_argument("-f", type=Path, help="Folder", default=Path("data/"))
-    parser.add_argument(
-        "-z",
-        type=float,
-        help="Focus plane height (z==0: on ground, z>0: above ground )",
-        default=0.0,
-    )
-    parser.add_argument(
-        "-r",
-        type=int,
-        help="Choose reference camera to stitch in, -1 for plane",
-        default=0,
-    )
-    parser.add_argument(
-        "-px-per-m",
-        type=float,
-        help="Resolution of virtual camera when r=-1",
-        default=1000,
-    )
-    args = parser.parse_args()
+    cfg = OmegaConf.merge(OmegaConf.load("oof.yml"), OmegaConf.from_cli())
 
-    # Load images, intrinsics and precomputed extrinsics
-    files = sorted(glob(str(args.f / "*.jpg")))[: args.n]
-    imgs = [cv2.imread(f, cv2.IMREAD_UNCHANGED) for f in files]
-    data = np.load(str(args.f / "data.npz"))
-    K_cam = data["K"]
-    t_cam_world = data["t_cam_world"][: args.n]
+    data = np.load(cfg.rawpath)
+    imgs = data["imgs"]
+    imgs = imgs.astype(np.float32) / 255
 
-    # Setup pi wrt. to world. The used chessboard is 4mm thick,
-    # so the pi is essentially offsetted 4mm in +z
-    t_world_plane = np.eye(4)
-    t_world_plane[2, 3] = 0.004 - args.z
+    weights = compute_weights_by_color(cfg, imgs)
 
-    if args.r >= 0:
-        # Stitch in physical camera
-        K_ref = K_cam
-        t_ref_world = t_cam_world[args.r]
-        extent = [-5000, 5000, -5000, 5000]
-    else:
-        # Stitch in virtual camera corresponding to pi
-        px_per_m = args.px_per_m
-        K_ref = np.eye(3)
-        K_ref[0, 0] = K_ref[1, 1] = px_per_m
+    # background
+    imgs = np.concatenate((imgs, np.ones_like(imgs[:1])), 0)
 
-        t_plane_ref = np.eye(4)
-        t_plane_ref[2, 3] = -1
-        t_ref_world = np.linalg.inv(t_world_plane @ t_plane_ref)
-        extent = [-0.5 * px_per_m, 1 * px_per_m, 0.0 * px_per_m, 1.5 * px_per_m]
-
-    img_c, K_c, xy_c, istack, _ = ist.stitch(
-        imgs,
-        K_cam,
-        t_cam_world,
-        t_world_plane,
-        K_ref,
-        t_ref_world,
-        extent=extent,
+    weights = np.concatenate(
+        (weights, np.clip(1.0 - weights.sum(0, keepdims=True), 0.0, 1.0)), 0
     )
 
-    istack = istack.astype(float) / 255.0
+    weights = weights / weights.sum(0, keepdims=True)
 
-    d = np.sqrt(3) - np.linalg.norm(
-        istack - np.array([0, 255, 255]).reshape(1, 1, 1, 3) / 255,
-        2,
-        axis=-1,
-        keepdims=True,
-    )
-    d[d < 0.7] = 1e-6
-    d /= d.sum(0, keepdims=True)
-
-    final = ((d * istack).sum(0) * 255).astype(np.uint8)
-
-    fig, ax = plt.subplots(figsize=plt.figaspect(img_c.shape[0] / img_c.shape[1]))
-    ax.imshow(img_c[..., ::-1], origin="upper")
+    out = ((weights * imgs).sum(0) * 255).astype(np.uint8)
+    fig, ax = plt.subplots(figsize=plt.figaspect(out.shape[0] / out.shape[1]))
+    ax.imshow(out[..., ::-1], origin="upper")
     ax.set_aspect("equal")
-
-    fig, ax = plt.subplots(figsize=plt.figaspect(img_c.shape[0] / img_c.shape[1]))
-    ax.imshow(final[..., ::-1], origin="upper")
-    ax.set_aspect("equal")
-
     plt.show()
+
+    # istack = istack.astype(float) / 255.0
+
+    # d = np.sqrt(3) - np.linalg.norm(
+    #     istack - np.array([0, 255, 255]).reshape(1, 1, 1, 3) / 255,
+    #     2,
+    #     axis=-1,
+    #     keepdims=True,
+    # )
+    # d[d < 0.7] = 1e-6
+    # d /= d.sum(0, keepdims=True)
+
+    # final = ((d * istack).sum(0) * 255).astype(np.uint8)
+
+    # fig, ax = plt.subplots(figsize=plt.figaspect(img_c.shape[0] / img_c.shape[1]))
+    # ax.imshow(img_c[..., ::-1], origin="upper")
+    # ax.set_aspect("equal")
+
+    # fig, ax = plt.subplots(figsize=plt.figaspect(img_c.shape[0] / img_c.shape[1]))
+    # ax.imshow(final[..., ::-1], origin="upper")
+    # ax.set_aspect("equal")
+
+    # plt.show()
 
 
 if __name__ == "__main__":
--- stitch.py
+++ stitch.py
@@ -6,57 +6,49 @@ import cv2
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.path import Path
+from matplotlib.path import Path as mplPath
+
+from omegaconf import OmegaConf
+from pathlib import Path
 
 import image_stitch as ist
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-n", type=int, help="Use first N images", default=-1)
-    parser.add_argument(
-        "-r",
-        type=int,
-        help="Choose reference camera to stitch in, -1 for plane",
-        default=0,
-    )
-    parser.add_argument(
-        "-px-per-m",
-        type=float,
-        help="Resolution of virtual camera when r=-1",
-        default=1000,
-    )
-    args = parser.parse_args()
+    cfg = OmegaConf.merge(OmegaConf.load("stitch.yml"), OmegaConf.from_cli())
+    base_path = Path(cfg.basepath)
 
     # Load images, intrinsics and precomputed extrinsics
-    files = sorted(glob("data/*.jpg"))[: args.n]
+    files = sorted(glob(str(base_path / "*.jpg")))[: cfg.nimages]
     imgs = [cv2.imread(f, cv2.IMREAD_UNCHANGED) for f in files]
-    data = np.load("data/data.npz")
+    data = np.load(str(base_path / "data.npz"))
     K_cam = data["K"]
-    t_cam_world = data["t_cam_world"][: args.n]
+    t_cam_world = data["t_cam_world"][: cfg.nimages]
 
     # Setup pi wrt. to world. The used chessboard is 4mm thick,
     # so the pi is essentially offsetted 4mm in +z
     t_world_plane = np.eye(4)
     t_world_plane[2, 3] = 0.004
 
-    if args.r >= 0:
+    mode = cfg[cfg.mode]
+    if mode.idx >= 0:
         # Stitch in physical camera
         K_ref = K_cam
-        t_ref_world = t_cam_world[args.r]
-        extent = [-5000, 5000, -5000, 5000]
+        t_ref_world = t_cam_world[mode.idx]
+        extent = list(mode.extent)
     else:
         # Stitch in virtual camera corresponding to pi
-        px_per_m = args.px_per_m
+        px_per_m = mode.px_per_m
         K_ref = np.eye(3)
         K_ref[0, 0] = K_ref[1, 1] = px_per_m
 
         t_plane_ref = np.eye(4)
         t_plane_ref[2, 3] = -1
+        t_world_plane[2, 3] += -mode.z  # -z towards ceiling
         t_ref_world = np.linalg.inv(t_world_plane @ t_plane_ref)
-        extent = [-3 * px_per_m, 5 * px_per_m, -1 * px_per_m, 2 * px_per_m]
+        extent = [i * mode.px_per_m for i in mode.extent]
 
-    img_c, K_c, xy_c, *_ = ist.stitch(
+    img_c, K_c, xy_c, w_imgs, w_weights = ist.stitch(
         imgs,
         K_cam,
         t_cam_world,
@@ -72,16 +64,16 @@ def main():
     ax.autoscale(False)
 
     codes = [
-        Path.MOVETO,
-        Path.LINETO,
-        Path.LINETO,
-        Path.LINETO,
-        Path.CLOSEPOLY,
+        mplPath.MOVETO,
+        mplPath.LINETO,
+        mplPath.LINETO,
+        mplPath.LINETO,
+        mplPath.CLOSEPOLY,
     ]
 
     xys = np.concatenate((xy_c, np.zeros((len(xy_c), 1, 2))), 1)  # (N,5,2)
 
-    paths = [Path(xy, codes) for xy in xys]
+    paths = [mplPath(xy, codes) for xy in xys]
     prop_cycle = plt.rcParams["axes.prop_cycle"]
     colors = prop_cycle.by_key()["color"]
     for idx, path in enumerate(paths):
@@ -96,7 +88,17 @@ def main():
         ax.add_patch(patch)
     now = time.strftime("%Y%m%d-%H%M%S")
     fig.savefig(f"tmp/stitch-{now}.png")
-    plt.show()
+    if cfg.show:
+        plt.show()
+    if cfg.save_raw:
+        np.savez_compressed(
+            f"tmp/stitch-{now}.npz",
+            img=img_c,
+            K=K_c,
+            xy=xy_c,
+            imgs=w_imgs,
+            weights=w_weights,
+        )
 
 
 if __name__ == "__main__":
