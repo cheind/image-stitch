98f766f0a726d7cd9e8d790e6c29818314cf7559
Christoph Heindl <christoph.heindl@gmail.com>
2024-10-14 17:20:13 +0200

    added colmap 2 stitch script start

---
+++ scripts/colmap2stitch.py
@@ -0,0 +1,153 @@
+from omegaconf import OmegaConf
+from pathlib import Path
+import numpy as np
+
+
+def parse_intrinsics(path: Path):
+    cameras = {}
+    with open(path, "r") as f:
+        for line in f:
+            # 1 SIMPLE_RADIAL 2048 1536 1580.46 1024 768 0.0045691
+            # 1 OPENCV 3840 2160 3178.27 3182.09 1920 1080 0.159668 -0.231286 -0.00123982 0.00272224
+            # 1 RADIAL 1920 1080 1665.1 960 540 0.0672856 -0.0761443
+            if line[0] == "#":
+                continue
+            els = line.split(" ")
+            camera = {}
+            camera_id = int(els[0])
+            camera["id"] = camera_id
+            camera["w"] = float(els[2])
+            camera["h"] = float(els[3])
+            camera["fl_x"] = float(els[4])
+            camera["fl_y"] = float(els[4])
+            camera["k1"] = 0
+            camera["k2"] = 0
+            camera["k3"] = 0
+            camera["k4"] = 0
+            camera["p1"] = 0
+            camera["p2"] = 0
+            camera["cx"] = camera["w"] / 2
+            camera["cy"] = camera["h"] / 2
+            camera["is_fisheye"] = False
+            if els[1] == "SIMPLE_PINHOLE":
+                camera["cx"] = float(els[5])
+                camera["cy"] = float(els[6])
+            elif els[1] == "PINHOLE":
+                camera["fl_y"] = float(els[5])
+                camera["cx"] = float(els[6])
+                camera["cy"] = float(els[7])
+            elif els[1] == "SIMPLE_RADIAL":
+                camera["cx"] = float(els[5])
+                camera["cy"] = float(els[6])
+                camera["k1"] = float(els[7])
+            elif els[1] == "RADIAL":
+                camera["cx"] = float(els[5])
+                camera["cy"] = float(els[6])
+                camera["k1"] = float(els[7])
+                camera["k2"] = float(els[8])
+            elif els[1] == "OPENCV":
+                camera["fl_y"] = float(els[5])
+                camera["cx"] = float(els[6])
+                camera["cy"] = float(els[7])
+                camera["k1"] = float(els[8])
+                camera["k2"] = float(els[9])
+                camera["p1"] = float(els[10])
+                camera["p2"] = float(els[11])
+            elif els[1] == "SIMPLE_RADIAL_FISHEYE":
+                camera["is_fisheye"] = True
+                camera["cx"] = float(els[5])
+                camera["cy"] = float(els[6])
+                camera["k1"] = float(els[7])
+            elif els[1] == "RADIAL_FISHEYE":
+                camera["is_fisheye"] = True
+                camera["cx"] = float(els[5])
+                camera["cy"] = float(els[6])
+                camera["k1"] = float(els[7])
+                camera["k2"] = float(els[8])
+            elif els[1] == "OPENCV_FISHEYE":
+                camera["is_fisheye"] = True
+                camera["fl_y"] = float(els[5])
+                camera["cx"] = float(els[6])
+                camera["cy"] = float(els[7])
+                camera["k1"] = float(els[8])
+                camera["k2"] = float(els[9])
+                camera["k3"] = float(els[10])
+                camera["k4"] = float(els[11])
+            else:
+                print("Unknown camera model ", els[1])
+
+        cameras[camera_id] = camera
+    return cameras
+
+
+def qvec2rotmat(qvec):
+    return np.array(
+        [
+            [
+                1 - 2 * qvec[2] ** 2 - 2 * qvec[3] ** 2,
+                2 * qvec[1] * qvec[2] - 2 * qvec[0] * qvec[3],
+                2 * qvec[3] * qvec[1] + 2 * qvec[0] * qvec[2],
+            ],
+            [
+                2 * qvec[1] * qvec[2] + 2 * qvec[0] * qvec[3],
+                1 - 2 * qvec[1] ** 2 - 2 * qvec[3] ** 2,
+                2 * qvec[2] * qvec[3] - 2 * qvec[0] * qvec[1],
+            ],
+            [
+                2 * qvec[3] * qvec[1] - 2 * qvec[0] * qvec[2],
+                2 * qvec[2] * qvec[3] + 2 * qvec[0] * qvec[1],
+                1 - 2 * qvec[1] ** 2 - 2 * qvec[2] ** 2,
+            ],
+        ]
+    )
+
+
+def parse_images(path: Path):
+    i = 0
+    images = {}
+    with open(path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if line[0] == "#":
+                continue
+            i = i + 1
+            if i % 2 == 0:
+                continue  # 3d points
+            elems = line.split(
+                " "
+            )  # 1-4 is quat, 5-7 is trans, 9ff is filename (9, if filename contains no spaces)
+
+            fname = "".join(elems[9:])
+            image_id = int(elems[0])
+            qvec = np.array(tuple(map(float, elems[1:5])))
+            tvec = np.array(tuple(map(float, elems[5:8])))
+            R = qvec2rotmat(-qvec)
+            t = tvec.reshape([3, 1])
+            m = np.concatenate(
+                [
+                    np.concatenate([R, t], 1),
+                    np.array([0.0, 0.0, 0.0, 1.0]).reshape([1, 4]),
+                ],
+                0,
+            )
+            images[image_id] = {"fname": fname, "t_cam_world": m}
+
+    return images
+
+
+def main():
+    cfg = OmegaConf.from_cli()
+
+    colmap_path = Path(cfg.colmapdir)
+    assert colmap_path.is_dir()
+
+    cams = parse_intrinsics(colmap_path / "cameras.txt")
+    images = parse_images(colmap_path / "images.txt")
+    print(cams)
+    print(images)
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
